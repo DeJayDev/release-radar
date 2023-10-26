f39284b39dd31f6eed5d98f9b816ad91266086cb
Dj Isaac <contact@dejaydev.com>
2023-10-25 20:37:09 -0500

    the rest of the owl

---
+++ README.md
@@ -0,0 +1,37 @@
+## Release Radar
+
+Not to be confused with the Spotify product of the same name :)
+
+### What is it?
+
+Release Radar checks GitHub Repos every 12 hours (configurable with `wrangler.toml`) and notifies you via Discord when a release is made.
+
+### How to use it?
+
+1. Create a worker (just deploy this repo)
+2. Create this KV namespace:
+   - `npx wrangler kv:namespace create repos`
+	 - `npx wrangler kv:namespace create repos --preview` (optional)
+3. Set the returned IDs appropriately `wrangler.toml`
+4. Define these secrets:
+   - `npx wrangler secret put DISCORD_WEBHOOK_URL`
+   - `npx wrangler secret put GITHUB_TOKEN` (optional)
+5. In your Cloudflare Dashboard for wherever you deployed this repo, navigate to the newly created KVs:
+   - **Workers & Pages** > **KV**
+6. Add the repos you want to target with **no value**. Just the key.
+
+	 Example:
+	 	<ul>
+	    <li>Key: `dejaydev/release-radar`</li>
+		  <li>Value: ``</li>
+		</ul>
+5. Redeploy this repo.
+6. wait.
+
+## License
+
+This project uses the [MIT License](./LICENSE), which allows you to freely use, copy, modify, and distribute the software for any purpose, with the condition that you include the original copyright notice and the MIT License text in your copies. However, there are also limitations: you cannot hold the copyright holders liable for any issues, remove or modify the MIT License text, or use the names of the copyright holders for endorsements without permission.
+
+## Minor Updates (Upon Request):
+
+Upon request, I may provide minor updates or improvements to this software. Please open an issue to get in touch.
--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,118 @@
-/**
- * Welcome to Cloudflare Workers!
- *
- * This is a template for a Scheduled Worker: a Worker that can run on a
- * configurable interval:
- * https://developers.cloudflare.com/workers/platform/triggers/cron-triggers/
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { request } from "@octokit/request";
+import { Endpoints } from "@octokit/types";
+import { RESTPostAPIWebhookWithTokenJSONBody } from 'discord-api-types/v10'
+import semver from 'semver';
 
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
-	//
-	// Example binding to a D1 Database. Learn more at https://developers.cloudflare.com/workers/platform/bindings/#d1-database-bindings
-	// DB: D1Database
+	repos: KVNamespace;
+	DISCORD_WEBHOOK_URL: string;
+	GITHUB_TOKEN?: string;
 }
 
 export default {
-	// The scheduled handler is invoked at the interval set in our wrangler.toml's
-	// [[triggers]] configuration.
 	async scheduled(event: ScheduledEvent, env: Env, ctx: ExecutionContext): Promise<void> {
-		// A Cron Trigger can make requests to other endpoints on the Internet,
-		// publish to a Queue, query a D1 Database, and much more.
-		//
-		// We'll keep it simple and make an API call to a Cloudflare API:
-		let resp = await fetch('https://api.cloudflare.com/client/v4/ips');
-		let wasSuccessful = resp.ok ? 'success' : 'fail';
+		if(!env.DISCORD_WEBHOOK_URL) {
+			console.log("No configured webhook URL. Stopping immediately.")
+			return; // not worth any effort.
+		}
 
-		// You could store this result in KV, write to a D1 Database, or publish to a Queue.
-		// In this template, we'll just log the result:
-		console.log(`trigger fired at ${event.cron}: ${wasSuccessful}`);
+		const kv = await env.repos.list();
+		const repos: string[] = kv.keys.map((key) => key.name);
+
+		const octokitSettings = {
+			headers: {
+				'X-GitHub-Api-Version': '2022-11-28',
+			}
+		}
+
+		if(env.GITHUB_TOKEN) {
+			(octokitSettings.headers as any)['Authorization'] = `Bearer ${env.GITHUB_TOKEN}`
+		}
+
+		for await (const repo of repos) {
+			let res = (await request(`GET /repos/${repo}/releases/latest`, {
+				...octokitSettings
+			})) as Endpoints['GET /repos/{owner}/{repo}/releases/latest']['response']
+
+			const release = res.data;
+			const latest = release.tag_name;
+
+			if(release.draft || release.prerelease) {
+				continue;
+			}
+
+			const current = await env.repos.get(repo);
+
+			let alert = false;
+
+			console.log("Current: " + current)
+			console.log("Latest: " + latest)
+			if(!current) {
+				env.repos.put(repo, latest);
+				ctx.waitUntil(sendPlainWebhook(`Now tracking ${repo} from ${latest}`, env));
+				continue
+			}
+
+			if(semver.valid(latest)) { // if we pulled a semver version
+				alert = semver.lt(current, latest);
+			} else {
+				alert = current !== latest;
+			}
+
+			if(!alert) {
+				continue;
+			}
+
+			env.repos.put(repo, latest);
+
+			await fetch(env.DISCORD_WEBHOOK_URL, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify({
+					content: `New release of ${repo}!`,
+					embeds: [{
+						title: repo,
+						fields: [
+							{
+								name: 'Released',
+								value: `<t:${new Date(release.published_at!).getTime() / 1000}:f>`,
+								inline: true
+							},
+							{
+								name: 'Version',
+								value: latest,
+								inline: true
+							}
+						],
+						timestamp: new Date().toISOString()
+					}]
+				} as RESTPostAPIWebhookWithTokenJSONBody)
+			});
+		}
+	},
+
+	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		return new Response('Hello, World!');
 	},
 };
+
+async function tell(message: string) {
+	console.log(message)
+}
+
+async function sendPlainWebhook(message: string, env: Env) {
+	console.log(env.DISCORD_WEBHOOK_URL);
+	const test = await fetch(env.DISCORD_WEBHOOK_URL, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+		body: JSON.stringify({
+			content: message,
+		})
+	})
+	console.log(await test.text())
+	console.log(message)
+}
